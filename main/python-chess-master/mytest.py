5cee314b6c946cb7db4181344a548dde158d0262
Pascal <pascalsbakker@gmail.com>
2017-03-21 11:54:13 -0400

    test

---
+++ main/python-chess-master/mytest.py
@@ -0,0 +1,35 @@
+
+import chess
+import random
+
+def randomAI(board): #Selects one of the legal moves randomly and returns it
+    moves = []
+    for move in board.legal_moves:
+        moves.append(move)
+    move = random.choice(moves)
+    while not chess.Move.from_uci(str(move)) in board.legal_moves:
+        move = random.choice(moves)
+
+    return chess.Move.from_uci(str(move))
+
+board = chess.Board()
+color = True #True=White False=Black
+num = 0 #Number of moves
+
+#Runs the chess game until win, draw, or stalemate
+while(True):
+    board.push(randomAI(board))
+    num=num+1
+    print board
+    if board.is_stalemate() or board.is_insufficient_material() or board.can_claim_threefold_repetition() or board.can_claim_fifty_moves() or board.can_claim_draw():
+        print "Stalemate or Draw"
+        print "Number of Moves: ",num
+        break;
+    if board.is_game_over() or board.is_checkmate():
+        if color:
+            print "White Won"
+        else:
+            print "Black Won"
+        print "Number of Moves: ",num
+        break;
+    color=not color
